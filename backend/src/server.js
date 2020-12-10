ea31b8b5e42848f4afaad91bc377ea4a1178da7e
Murillo Borges <murillo_borgess@hotmail.com>
2020-12-09 21:14:29 -0300

    :construction_worker: Update CI build system
    Realizando ajustes para deploy automatico

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 import app from './app';
 import logger from './middlewares/logger';
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   logger.info('Server runing');
   console.log('Servidor está rodando na porta: 3333');
 });
